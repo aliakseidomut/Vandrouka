9e2a4c04a84d7c2146f5302b5d5c86329f30e352
magicText0011 <magictext0011@gmail.com>
2026-04-25 11:43:41 +0300

    init

--- server/prisma/migrations/20260304120000_add_google_auth/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "googleId" TEXT,
-ADD COLUMN "name" TEXT,
-ADD COLUMN "picture" TEXT,
-ADD COLUMN "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ALTER COLUMN "password" DROP NOT NULL;
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
